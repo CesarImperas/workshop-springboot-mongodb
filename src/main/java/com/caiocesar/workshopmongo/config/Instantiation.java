b75dff89ca936a50ae40e7861563341162103167
Caio Cesar <caiocesarvieiracavalcanti@gmail.com>
2024-11-09 13:34:23 -0300

    Author data projection with DTO

--- src/main/java/com/caiocesar/workshopmongo/config/Instantiation.java
+++ src/main/java/com/caiocesar/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.caiocesar.workshopmongo.domain.Post;
 import com.caiocesar.workshopmongo.domain.User;
+import com.caiocesar.workshopmongo.dto.AuthorDTO;
 import com.caiocesar.workshopmongo.repository.PostRepository;
 import com.caiocesar.workshopmongo.repository.UserRepository;
 
@@ -36,11 +37,13 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com"); 
 		User bob = new User(null, "Bob Grey", "bob@gmail.com"); 
 	
-		Post post1 = new Post(null, LocalDate.parse("21/03/2018", form).atStartOfDay(ZoneId.of("GMT")).toInstant(), "Partiu viagem", "Vou viajar para São Paulo", maria);
-		Post post2 = new Post(null, LocalDate.parse("23/03/2018", form).atStartOfDay(ZoneId.of("GMT")).toInstant(), "Bom dia", "Acordei feliz hoje!", maria);
+		// Persistência dos dados do tipo User
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, LocalDate.parse("21/03/2018", form).atStartOfDay(ZoneId.of("GMT")).toInstant(), "Partiu viagem", "Vou viajar para São Paulo", new AuthorDTO(maria));
+		Post post2 = new Post(null, LocalDate.parse("23/03/2018", form).atStartOfDay(ZoneId.of("GMT")).toInstant(), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		// Salvar os objetos como dados no banco de dados MongoDB
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 }
