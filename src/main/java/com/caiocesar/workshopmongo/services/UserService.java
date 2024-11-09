6e41020f507aca49ddc1ae9af65626e1c64ffe24
Caio Cesar <caiocesarvieiracavalcanti@gmail.com>
2024-11-08 23:31:26 -0300

    User insertion with POST

--- src/main/java/com/caiocesar/workshopmongo/services/UserService.java
+++ src/main/java/com/caiocesar/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.caiocesar.workshopmongo.domain.User;
+import com.caiocesar.workshopmongo.dto.UserDTO;
 import com.caiocesar.workshopmongo.repository.UserRepository;
 import com.caiocesar.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
