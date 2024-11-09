0d0243921395a08b6a6fc19e2c39a8e967eb489e
Caio Cesar <caiocesarvieiracavalcanti@gmail.com>
2024-11-08 23:15:01 -0300

    Method findById user

---
+++ src/main/java/com/caiocesar/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.caiocesar.workshopmongo.resources.exception;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.caiocesar.workshopmongo.services.exception.ObjectNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		String msg = "Object not found!";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		
+		StandardError err = new StandardError(Instant.now(), status.value(), msg, e.getMessage(), request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+	}
+}
--- src/main/java/com/caiocesar/workshopmongo/services/UserService.java
+++ src/main/java/com/caiocesar/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.caiocesar.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.caiocesar.workshopmongo.domain.User;
 import com.caiocesar.workshopmongo.repository.UserRepository;
+import com.caiocesar.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return userRepository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
 	}
 }
